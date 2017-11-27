838303a8ba53d042d6900fcc732074a57813e4db
ljh257110 <15764219379@163.com>
2017-11-27 19:20:00 +0800

    添加识别网络状态功能

--- pages/find/find.js
+++ pages/find/find.js
@@ -11,7 +11,6 @@ Page({
     videobool:false,
     file:"",
     filebool:false,
-    isConnected:false,
     networkType:"none",
     netbool:false
   },
@@ -97,14 +96,16 @@ Page({
   },
   netInfo(){
     var self= this;
-    wx.onNetworkStatusChange(function (res) {
-      console.log(res.isConnected)
-      console.log(res.networkType)
-      self.setData({
-        isConnected: res.isConnected,
-        networkType: res.networkType,
-        netbool:true
-      })
+    wx.getNetworkType({
+      success: function (res) {
+        // 返回网络类型, 有效值：
+        // wifi/2g/3g/4g/unknown(Android下不常见的网络类型)/none(无网络)
+        var networkType = res.networkType
+        self.setData({
+          networkType: networkType,
+          netbool: true
+        })
+      }
     })
   },
   netcancel(){
