c5b70640c6e1827fc2f59a4050c4a020a3b579ec
ljh257110 <15764219379@163.com>
2017-11-27 16:46:40 +0800

    添加图片获取功能

--- pages/find/find.js
+++ pages/find/find.js
@@ -5,9 +5,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    src:"",
+    bool:false
   },
-
+  findpic(){
+    var self = this;
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        
+        var tempFilePaths = res.tempFilePaths;
+        console.log(tempFilePaths[0])
+        self.setData({
+          src: tempFilePaths[0],
+          bool:true
+        })
+      }
+    })
+  },
+  cancel(){
+    this.setData({
+      bool: false
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
