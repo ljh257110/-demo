0abb4b30757bc65f1d5751f4825170c2a5b8ac09
ljh257110 <15764219379@163.com>
2017-11-27 19:50:57 +0800

    添加轮播图

--- pages/find/find.js
+++ pages/find/find.js
@@ -12,7 +12,37 @@ Page({
     file:"",
     filebool:false,
     networkType:"none",
-    netbool:false
+    netbool:false,
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    circular:true
+  },
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
   },
   findpic(){
     var self = this;
