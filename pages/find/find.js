b6a27b16b25bba6be97185b404d6fd6613511c04
ljh257110 <15764219379@163.com>
2017-11-27 17:58:57 +0800

    添加获取位置信息功能

--- pages/find/find.js
+++ pages/find/find.js
@@ -81,6 +81,20 @@ Page({
       filebool: false
     })
   },
+  locationInfo(){
+    wx.getLocation({
+      type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        wx.openLocation({
+          latitude: latitude,
+          longitude: longitude,
+          scale: 28
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
